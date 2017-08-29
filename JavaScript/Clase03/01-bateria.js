ee2d5fad2235ccc454f17989fb21c884f7d2d887
Sergio <shidalgo@outlook.com>
2017-08-29 10:30:48 -0500

    Clase03

--- JavaScript/Clase03/01-bateria.js
+++ JavaScript/Clase03/01-bateria.js
@@ -1,8 +1,9 @@
+// Variable de tipo función
 const arregloTeclas = document.querySelectorAll("a.tecla")
 arregloTeclas.forEach( elemento => {
 	elemento.addEventListener("transitionend", function(e){
 		e.target.classList.remove("presionada") // Esto también funciona
-		// this.classList.remove("presionada")
+		// this.classList.remove("presionada") // Esta es una alternativa
 	})
 
 	/*elemento.addEventListener("transitionend", e => {
@@ -11,22 +12,25 @@ arregloTeclas.forEach( elemento => {
 	})*/
 })
 
-window.addEventListener("keydown", evt => {
+
+window.addEventListener("keydown", evt => { // El evento "keydown" sucede cuando se hunde una tecla
 	// console.log(evt)
-	const teclaPresionada = evt.keyCode
+	const teclaPresionada = evt.keyCode  // Devuelve el código ASCII de la tecla presionada
 
 	const tecla = document.querySelector(`a[data-tecla="${teclaPresionada}"]`)
 	
 	if(tecla){
 		arregloTeclas.forEach(item => {
-			item.classList.remove("presionada")
+			item.classList.remove("presionada")   // El método "remove" quita una clase de un elemento
 		})
-		tecla.classList.add("presionada")
+		tecla.classList.add("presionada")  // El método "add" agrega una clase a un elemento
 
+		// Selección de un elemento usando un atributo
 		const audioAEjecutar = document.querySelector(`audio[data-tecla="${teclaPresionada}"]`)
 
-		audioAEjecutar.currentTime = 0
-		audioAEjecutar.play()
+		audioAEjecutar.currentTime = 0  // Hay que poner el puntero del sonido al comienza para reiniciar el sonido. De otra forma habrá que esperar que el sonido termine de ejecutarse para ejecutarlo otra vez
+
+		audioAEjecutar.play() // Reproduce un audio
 	}
 	/*
 		a 65
--- JavaScript/Clase03/02-reloj.js
+++ JavaScript/Clase03/02-reloj.js
@@ -1,26 +1,32 @@
+// Asigna referencias de las manecillas a variables
 const segundero = document.querySelector("div.segundero")
 const minutero = document.querySelector("div.minutero")
 const horario = document.querySelector("div.horario")
 
+// Obtiene la fecha y la hora actual
 const fechaHora = new Date()
-const horas = fechaHora.getHours()
-const minutos = fechaHora.getMinutes()
-const segundos = fechaHora.getSeconds()
+const horas = fechaHora.getHours() // Obtiene las horas de la variable anterior
+const minutos = fechaHora.getMinutes() // Obtiene los minutos de la variable anterior
+const segundos = fechaHora.getSeconds() // Obtiene los segundos de la variable anterior
 
+// Asigna a variables los grados que se mueven cada una de las manecillas por segundo
 const dAngSegundero = 6
 const dAngMinutero = .1
 const dAngHorario = 0.00833333333
 
+// Calcula el ángulo de giro inicial de cada manecilla
 let anguloSegundero = -90 + segundos*dAngSegundero
 let anguloMinutero = -90 + (minutos*60 + segundos)*dAngMinutero 
 let anguloHorario = -90 + (horas*60*60 + minutos*60 + segundos)*dAngHorario
 
+// Mueve las manecillas del reloj
 const moverManecillas = () => {
 	segundero.style.transform = `rotate(${anguloSegundero}deg)`
 	minutero.style.transform = `rotate(${anguloMinutero}deg)`
 	horario.style.transform = `rotate(${anguloHorario}deg)` 
 }
 
+// Cada segundo modifica el ángulo de cada manecilla cada segundo
 setInterval(()=>{
 	anguloSegundero += dAngSegundero
 	anguloMinutero += dAngMinutero
@@ -28,8 +34,10 @@ setInterval(()=>{
 	moverManecillas()
 }, 1000)
 
+// Move las manecillas
 moverManecillas()
 
+// Agrega transiciones iniciales a las manecillas
 segundero.classList.add("transicionManecillas")
 minutero.classList.add("transicionManecillas")
 horario.classList.add("transicionManecillas")
